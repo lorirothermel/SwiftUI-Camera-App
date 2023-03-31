68aa47349570996fc71ff2d86229ec05e46fd61d
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-03-31 01:06:49 -0400

    Camera Demo - Still need to save to photo library

--- SwiftUI Camera App/ContentView.swift
+++ SwiftUI Camera App/ContentView.swift
@@ -6,18 +6,61 @@
 //
 
 import SwiftUI
+import PhotosUI
+
 
 struct ContentView: View {
+    @State private var showSheet: Bool = false
+    @State private var showImagePicker: Bool = false
+    @State private var sourceType: UIImagePickerController.SourceType = .camera
+    @State private var image: UIImage?
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            VStack {
+                      
+                
+                Image(uiImage: image ?? UIImage(named: "placeholder")!)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                    .foregroundColor(.blue)
+
+                Button("Choose Picture") {
+                    showSheet = true
+                    
+                }
+                .padding()
+                .actionSheet(isPresented: $showSheet) {
+                    ActionSheet(title: Text("Select Photo"),
+                                message: Text("Choose"),
+                                buttons: [
+                                            .default(Text("Photo Library")) {
+                                                showImagePicker = true
+                                                self.sourceType = .photoLibrary
+                                            },
+                                            .default(Text("Camera")) {
+                                                showImagePicker = true
+                                                self.sourceType = .camera
+                                            },
+                                            .cancel()
+                                         ])
+                }
+                        
+            }  // VStack
+            .navigationTitle("Camera Demo")
+                
+        }  // NavigationStack
+        .sheet(isPresented: $showImagePicker) {
+            ImagePicker(image: self.$image, isShown: self.$showImagePicker, sourceType: self.sourceType)
         }
-        .padding()
-    }
-}
+    }  // some View
+}  // ContentView
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
